8e0e8b7ee1e1b9665014ec0c93c45767e13ed78b
Lawrence Tam <lawrenceatam@gmail.com>
2021-05-09 00:42:40 -0700

    first commit

---
+++ lesson_1/practice.js
@@ -0,0 +1,28 @@
+function createBook(title, author, read = false) {
+  return {
+    title,
+    author,
+    read,
+
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    },
+    readBook() {
+      this.read = true;
+    }
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris', true);
+let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse', false);
+
+console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
+console.log(book2.getDescription());  // "Me Talk Pretty One Day was written by David Sedaris."
+console.log(book3.getDescription());  // "Aunts aren't Gentlemen was written by PG Wodehouse"
+
+console.log(book1.read); // => false
+console.log(book2.read); // => true
+console.log(book3.read); // => false
+book3.readBook();
+console.log(book3.read);
